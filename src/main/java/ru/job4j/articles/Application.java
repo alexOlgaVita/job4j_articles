5399977f6912b9fa5d560c992944a0f760c52dcb
olga.ilyina <olga.ilyina@axenix.pro>
2024-12-08 12:59:32 +0300

    3. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/Application.java
+++ src/main/java/ru/job4j/articles/Application.java
@@ -18,11 +18,8 @@ public class Application {
 
     public static void main(String[] args) {
         var properties = loadProperties();
-        var wordStore = new WordStore(properties);
-        var articleStore = new ArticleStore(properties);
-        var articleGenerator = new RandomArticleGenerator();
-        var articleService = new SimpleArticleService(articleGenerator);
-        articleService.generate(wordStore, TARGET_COUNT, articleStore);
+        var articleService = new SimpleArticleService(new RandomArticleGenerator());
+        articleService.generate(new WordStore(properties), TARGET_COUNT, new ArticleStore(properties));
     }
 
     private static Properties loadProperties() {
--- src/main/java/ru/job4j/articles/model/Word.java
+++ src/main/java/ru/job4j/articles/model/Word.java
@@ -26,4 +26,25 @@ public class Word {
     public void setValue(String value) {
         this.value = value;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 17;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Word word = (Word) obj;
+        return (id == word.id) && (value.equals(word.value));
+    }
 }
